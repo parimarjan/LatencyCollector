01695d36fe5246fa8b2ce52e81de5d8c22c84305
parimarjan <parimarjannegi@gmail.com>
2023-02-28 21:30:46 -0500

    updated ergast small

---
+++ queries/ergast-small/1325164948183818191827189057436808865043449270656.sql
@@ -0,0 +1,19 @@
+SELECT COUNT(*)
+FROM
+constructors c,
+drivers d,
+results res,
+constructors c2,
+drivers d2,
+results res2
+WHERE c."constructorId" = res."constructorId"
+AND res."driverId" = d."driverId"
+AND d.nationality != c.nationality
+AND c2."constructorId" = res2."constructorId"
+AND res2."driverId" = d2."driverId"
+AND d2.nationality = c2.nationality
+AND d2."driverId" = d."driverId"
+AND (res.position IN ('13.0','17.0','29.0','9.0'))
+AND (res2.position IN ('7.0'))
+AND (res.points IN ('1.0','4.0') OR res.points IS NULL)
+AND (res2.points IN ('1.0','6.0'))
---
+++ queries/ergast-small/921921613163875006473772965225740488782749635300.sql
@@ -0,0 +1,28 @@
+SELECT ps."driverId", MAX(ps."milliseconds")
+FROM results res1,
+results res2,
+status s1,
+status s2,
+drivers d1,
+drivers d2,
+races r1,
+races r2,
+"pitStops" ps
+WHERE
+ps."driverId" = d1."driverId"
+AND ps."driverId" = d2."driverId"
+AND ps."driverId" = res1."driverId"
+AND res1."raceId" = r1."raceId"
+AND res2."raceId" = r2."raceId"
+AND res1."statusId" = s1."statusId"
+AND res2."statusId" = s2."statusId"
+AND d1."driverId" = res1."driverId"
+AND d2."driverId" = res2."driverId"
+AND d1."driverId" = d2."driverId"
+AND (s1.status NOT LIKE ('Collision'))
+AND (res1.position <= '1.0')
+AND (res1."positionOrder" <= '1')
+AND (s2.status NOT LIKE ('Not classified'))
+AND (res2.position > '11.0')
+AND (res2."positionOrder" <= '14')
+GROUP BY ps."raceId", ps."driverId" ORDER BY MAX(ps."milliseconds")
