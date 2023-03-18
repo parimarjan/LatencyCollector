ecc4631a5ed3e97e5056f9c207384b80db761954
parimarjan <parimarjannegi@gmail.com>
2023-03-18 16:19:09 -0400

    all

--- scripts/linux_workloads/run_all.sh
+++ scripts/linux_workloads/run_all.sh
@@ -4,3 +4,9 @@ run_single.sh cpp_bench.sh
 run_single.sh pbmc.sh
 run_single.sh datasci_bench.sh
 run_single.sh sentences.sh
+run_single.sh matrix.sh
+run_single.sh ansibench.sh
+run_single.sh learned_sort.sh
+run_single.sh redis.sh
+run_single.sh iozone.sh
+run_single.sh interbench.sh
