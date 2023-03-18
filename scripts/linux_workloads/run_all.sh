78ee9b44db2d1745751a6fe0a810b98b1aa88851
parimarjan <parimarjannegi@gmail.com>
2023-03-18 18:11:01 -0400

    fix

--- scripts/linux_workloads/run_all.sh
+++ scripts/linux_workloads/run_all.sh
@@ -1,12 +1,12 @@
 
-./run_single.sh lxml.sh
-./run_single.sh cpp_bench.sh
-./run_single.sh pbmc.sh
-./run_single.sh datasci_bench.sh
-./run_single.sh sentences.sh
-./run_single.sh matrix.sh
-./run_single.sh ansibench.sh
-./run_single.sh learned_sort.sh
-./run_single.sh redis.sh
-./run_single.sh iozone.sh
-./run_single.sh interbench.sh
+bash run_single.sh lxml.sh
+bash run_single.sh cpp_bench.sh
+bash run_single.sh pbmc.sh
+bash run_single.sh datasci_bench.sh
+bash run_single.sh sentences.sh
+bash run_single.sh matrix.sh
+bash run_single.sh ansibench.sh
+bash run_single.sh learned_sort.sh
+bash run_single.sh redis.sh
+bash run_single.sh iozone.sh
+bash run_single.sh interbench.sh
