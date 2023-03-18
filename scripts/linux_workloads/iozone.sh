9bdb96f4106020972709484e4de41f3778a7738f
parimarjan <parimarjannegi@gmail.com>
2023-03-18 14:33:59 -0400

    all shells

---
+++ scripts/linux_workloads/iozone.sh
@@ -0,0 +1,7 @@
+# docs at: https://github.com/pantheon-systems/iozone/blob/master/docs/IOzone_msword_98.pdf
+wget https://www.iozone.org/src/current/iozone3_494.tgz
+tar -xvf iozone3_494.tgz
+cd iozone3_494/src/current
+./iozone -a
+./iozone -A
+./iozone -az -i 0 -i 1
---
+++ scripts/linux_workloads/run_all.sh
@@ -0,0 +1,6 @@
+
+run_single.sh lxml.sh
+run_single.sh cpp_bench.sh
+run_single.sh pbmc.sh
+run_single.sh datasci_bench.sh
+run_single.sh sentences.sh
