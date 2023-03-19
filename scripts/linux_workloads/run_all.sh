0765e9c6474928566ce77cb7dd7d8e1f08864677
parimarjan <parimarjannegi@gmail.com>
2023-03-19 01:36:16 -0400

    runall update

--- scripts/linux_workloads/run_all.sh
+++ scripts/linux_workloads/run_all.sh
@@ -1,3 +1,8 @@
+bash run_single.sh redis.sh
+bash run_single.sh iozone.sh
+bash run_single.sh interbench.sh
+bash run_single.sh memtier.sh
+bash run_single.sh hayden.sh
 bash run_single.sh lxml.sh
 bash run_single.sh cpp_bench.sh
 bash run_single.sh pbmc.sh
@@ -6,14 +11,9 @@ bash run_single.sh sentences.sh
 bash run_single.sh matrix.sh
 bash run_single.sh ansibench.sh
 bash run_single.sh learned_sort.sh
-bash run_single.sh redis.sh
-bash run_single.sh iozone.sh
-bash run_single.sh interbench.sh
 bash run_single.sh percolation.sh
 ## fix bench script
 bash run_single.sh radix.sh
-bash run_single.sh hayden.sh
-bash run_single.sh memtier.sh
 
 ## TODO:
 #db_bench.sh
