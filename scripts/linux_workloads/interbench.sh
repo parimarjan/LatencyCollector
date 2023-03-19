fc743f176aa944b1f33c77e25ec0307d28819e33
parimarjan <parimarjannegi@gmail.com>
2023-03-19 00:10:28 -0400

    minor

--- scripts/linux_workloads/interbench.sh
+++ scripts/linux_workloads/interbench.sh
@@ -3,7 +3,8 @@ git clone https://github.com/parimarjan/interbench.git
 cd interbench
 make
 
-./interbench -t 90
-./interbench -t 90 -r
-./interbench -t 90 -C 29
+sudo ./interbench
+sudo ./interbench -t 90
+sudo ./interbench -t 90 -r
+sudo ./interbench -t 90 -C 29
 ## TODO: put only custom video/audio/etc load as background tasks
