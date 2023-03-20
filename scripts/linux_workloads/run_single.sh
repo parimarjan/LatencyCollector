efd17af812455c5d5b04b8ee88853fe732ff2ff8
parimarjan <parimarjannegi@gmail.com>
2023-03-20 18:24:10 -0400

    fixed things

--- scripts/linux_workloads/run_single.sh
+++ scripts/linux_workloads/run_single.sh
@@ -8,7 +8,11 @@ mkdir -p ~/log/results/perf
 OUTDIR=~/log/results/perf/
 rid=$RANDOM
 
-E=dTLB-loads,iTLB-loads,instructions,cache-misses,cache-references,cpu-clock,task-clock,page-faults,minor-faults,major-faults,cs,cpu-migrations,alignment-faults,branch-load-misses,branch-loads,bus-cycles,idle-cycles-backend,L1-dcache-load-misses,L1-dcache-store-misses,L1-icache-load-misses,LLC-load-misses
+#E=dTLB-loads,iTLB-loads,instructions,cache-misses,cache-references,task-clock,page-faults,minor-faults,major-faults,cs,cpu-migrations,alignment-faults,branch-load-misses,branch-loads,bus-cycles,idle-cycles-backend,L1-dcache-load-misses,L1-dcache-store-misses,L1-icache-load-misses,LLC-load-misses
+
+E=cycles,instructions,cache-misses,page-faults,branch-misses,context-switches,L1-dcache-load-misses,LLC-load-misses
+
+#block:block_rq_issue
 
 while read in;
   do
@@ -39,12 +43,16 @@ while read in;
       cmdname=$(echo -n "$cmdname" | sha256sum | awk '{print $1}')
       plogs=${OUTDIR}${cmdname}'.csv'
       plogs_err=${plots}.stderr
-      ts=`date +%s`
-      echo "$cmdname,$in,$FN,$ts" >> ${OUTDIR}allcommands.csv
     fi
     echo "cmd name: " $cmdname
     echo "outdir: " $OUTDIR
     cmd="perf stat -x, -e ${E} -o $plogs $in"
     echo "Going to evaluate: " $cmd
+
+    ts=`date +%s`
     eval $cmd
+    status=$?
+    endts=`date +%s`
+    exectime="$((endts-ts))"
+    echo "$cmdname,$in,$FN,$ts,$exectime,$status" >> ${OUTDIR}allcommands.csv
 done < $FN
