26fe18dd32896d468dbfaf154bdf77effea9245e
Ubuntu <ubuntu@ip-172-31-23-46.us-east-2.compute.internal>
2023-03-20 22:49:53 +0000

    fixed fio

--- scripts/linux_workloads/fio.sh
+++ scripts/linux_workloads/fio.sh
@@ -1,48 +1,37 @@
 mkdir -p fio
-cd cio
+cd fio
 
-#fio --cmdhelp=rw
-                  #rw: IO direction
-               #alias: readwrite
-                #type: string (opt=bla)
-             #default: read
-        #valid values: read       Sequential read
-                    #: write      Sequential write
-                    #: trim       Sequential trim
-                    #: randread   Random read
-                    #: randwrite  Random write
-                    #: randtrim   Random trim
-                    #: rw         Sequential read and write mix
-                    #: readwrite  Sequential read and write mix
-                    #: randrw     Random read and write mix
-                    #: trimwrite  Trim and write mix, trims preceding writes
-
-SIZE1=100mb
-SIZE2=4gb
-SIZE3=8gb
-KINDS=(read write trime randread randwrite randtrim rw readwrite randrw trimwrite)
-
-for kind in "${KINDS[@]}"
-  do
-  CMD1="sudo fio --filename=./test --rw=$kind --direct=1 --bs=1M \
-  --ioengine=libaio --numjobs=1 --group_reporting --name=$kind \
-  --iodepth=16 --size=$SIZE1"
-  echo $CMD1
-  eval $CMD1
-
-  CMD2="sudo fio --filename=./test --rw=$kind --direct=1 --bs=1M \
-  --ioengine=libaio --numjobs=1 --group_reporting --name=$kind \
-  --iodepth=16 --size=$SIZE2"
-  echo $CMD2
-  eval $CMD2
-
-  CMD3="sudo fio --filename=./test --rw=$kind --direct=1 --bs=1M \
-  --ioengine=libaio --numjobs=1 --group_reporting --name=$kind \
-  --iodepth=16 --size=$SIZE3"
-  echo $CMD3
-  eval $CMD3
-done
-
+sudo fio --filename=./test --rw=read --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=read --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=read --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=read --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=read --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=read --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=write --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=write --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=write --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=write --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=write --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=write --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=trime --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trime --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=trime --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trime --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=trime --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trime --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=randread --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randread --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=randread --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randread --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=randread --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randread --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=randwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randwrite --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=randwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randwrite --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=randwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randwrite --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=randtrim --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randtrim --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=randtrim --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randtrim --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=randtrim --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randtrim --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=rw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=rw --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=rw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=rw --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=rw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=rw --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=readwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=readwrite --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=readwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=readwrite --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=readwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=readwrite --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=randrw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randrw --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=randrw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randrw --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=randrw --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=randrw --iodepth=16 --size=8gb
+sudo fio --filename=./test --rw=trimwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trimwrite --iodepth=16 --size=100mb
+sudo fio --filename=./test --rw=trimwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trimwrite --iodepth=16 --size=4gb
+sudo fio --filename=./test --rw=trimwrite --direct=1 --bs=1M --ioengine=libaio --numjobs=1 --group_reporting --name=trimwrite --iodepth=16 --size=8gb
 
 cd ..
 rm -rf fio
+
