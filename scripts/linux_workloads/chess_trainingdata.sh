ca34e94a2aa7b9aad74e84d409b264da5e5d6da5
parimarjan <parimarjannegi@gmail.com>
2023-03-21 01:40:36 -0400

    chesstraining data

---
+++ scripts/linux_workloads/chess_trainingdata.sh
@@ -0,0 +1,16 @@
+
+git clone --recursive https://github.com/parimarjan/trainingdata-tool.git
+cd trainingdata-tool/
+mkdir -p build
+git submodule sync --recursive
+git submodule update --recursive --init
+cmake -H. -Bbuild
+cmake --build build
+
+wget https://database.lichess.org/standard/lichess_db_standard_rated_2023-02.pgn.zst
+unzstd lichess_db_standard_rated_2023-02.pgn.zst
+
+## TODO: pgn-extract
+
+./build/trainingdata-tool lichess_db_standard_rated_2023-02.pgn
+
---
+++ scripts/linux_workloads/scoutfish.sh
@@ -0,0 +1,11 @@
+
+git clone https://github.com/mcostalba/scoutfish.git
+cd scoutfish/src
+
+wget https://database.lichess.org/standard/lichess_db_standard_rated_2023-02.pgn.zst
+unzstd lichess_db_standard_rated_2023-02.pgn.zst
+
+make build ARCH=x86-64 COMP=g++
+
+./scoutfish make lichess_db_standard_rated_2023-02.pgn
+./scoutfish scout lichess_db_standard_rated_2023-02.scout {"sub-fen": "8/8/p7/8/8/1B3N2/8/8"}
