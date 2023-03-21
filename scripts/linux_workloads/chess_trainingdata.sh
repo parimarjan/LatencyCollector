e992bdfb91224773388169e2b4a6bbada7c598dd
parimarjan <parimarjannegi@gmail.com>
2023-03-21 12:21:13 -0400

    fixes, changes

--- scripts/linux_workloads/chess_trainingdata.sh
+++ scripts/linux_workloads/chess_trainingdata.sh
@@ -15,43 +15,62 @@ cd ../..
 wget https://theweekinchess.com/zips/twic1474g.zip
 unzip twic1474g.zip
 rm twic1474g.zip
+cat twic1474.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 > test.pgn
 ./build/trainingdata-tool twic1474.pgn
-rm twic1474.pgn
 
-wget https://theweekinchess.com/zips/twic1000g.zip
-unzip twic1000g.zip
-rm twic1000g.zip
-./build/trainingdata-tool twic1000.pgn
-rm twic1000.pgn
+#rm twic1474.pgn
+#rm test.pgn
 
+#wget https://theweekinchess.com/zips/twic1000g.zip
+#unzip twic1000g.zip
+#rm twic1000g.zip
+#./build/trainingdata-tool twic1000.pgn
+#rm twic1000.pgn
 
 wget https://database.lichess.org/standard/lichess_db_standard_rated_2013-02.pgn.zst
 unzstd lichess_db_standard_rated_2013-02.pgn.zst
 rm lichess_db_standard_rated_2013-02.pgn.zst
-cat lichess_db_standard_rated_2013-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
-./build/trainingdata-tool lichess_db_standard_rated_2013-02.pgn
+cat lichess_db_standard_rated_2013-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 > test.pgn
+./build/trainingdata-tool test.pgn
 rm lichess_db_standard_rated_2013-02.pgn
+rm test.pgn
 
-wget https://database.lichess.org/standard/lichess_db_standard_rated_2014-02.pgn.zst
-unzstd lichess_db_standard_rated_2014-02.pgn.zst
-rm lichess_db_standard_rated_2014-02.pgn.zst
-cat lichess_db_standard_rated_2014-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
-./build/trainingdata-tool lichess_db_standard_rated_2014-02.pgn
-rm lichess_db_standard_rated_2014-02.pgn
+#wget https://database.lichess.org/standard/lichess_db_standard_rated_2014-02.pgn.zst
+#unzstd lichess_db_standard_rated_2014-02.pgn.zst
+#rm lichess_db_standard_rated_2014-02.pgn.zst
+#cat lichess_db_standard_rated_2014-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 > test.pgn
+#./build/trainingdata-tool test.pgn
+#rm lichess_db_standard_rated_2014-02.pgn
+#rm test.pgn
 
 wget https://database.lichess.org/standard/lichess_db_standard_rated_2015-02.pgn.zst
 unzstd lichess_db_standard_rated_2015-02.pgn.zst
 rm lichess_db_standard_rated_2015-06.pgn.zst
-cat lichess_db_standard_rated_2015-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
+cat lichess_db_standard_rated_2015-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 > test.pgn
 ./build/trainingdata-tool lichess_db_standard_rated_2015-02.pgn
 rm lichess_db_standard_rated_2015-02.pgn
+rm test.pgn
 
-#wget https://database.lichess.org/standard/lichess_db_standard_rated_2016-02.pgn.zst
-#unzstd lichess_db_standard_rated_2016-02.pgn.zst
-#rm lichess_db_standard_rated_2016-02.pgn.zst
-#cat lichess_db_standard_rated_2016-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
-#./build/trainingdata-tool lichess_db_standard_rated_2016-02.pgn
-#rm lichess_db_standard_rated_2016-02.pgn
+##wget https://database.lichess.org/standard/lichess_db_standard_rated_2016-02.pgn.zst
+##unzstd lichess_db_standard_rated_2016-02.pgn.zst
+##rm lichess_db_standard_rated_2016-02.pgn.zst
+##cat lichess_db_standard_rated_2016-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
+##./build/trainingdata-tool lichess_db_standard_rated_2016-02.pgn
+##rm lichess_db_standard_rated_2016-02.pgn
 
-cd ..
-rm -rf trainingdata-tool
+#while read in;
+  #do
+    #if [ -z "$in" ]; then
+      #continue
+    #fi
+  #wget https://database.lichess.org/standard/lichess_db_standard_rated_2013-02.pgn.zst
+  #unzstd lichess_db_standard_rated_2013-02.pgn.zst
+  #rm lichess_db_standard_rated_2013-02.pgn.zst
+  #cat lichess_db_standard_rated_2013-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
+  #./build/trainingdata-tool lichess_db_standard_rated_2013-02.pgn
+  #rm lichess_db_standard_rated_2013-02.pgn
+#done < pgnlinks.txt
+
+#cd ..
+#rm -rf trainingdata-tool
+
--- scripts/linux_workloads/ffmpeg.sh
+++ scripts/linux_workloads/ffmpeg.sh
@@ -1,24 +1,64 @@
 
 mkdir -p ffmpeg
+cd ffmpeg
 
 youtube-dl -o input.mp4 https://www.dailymotion.com/video/x17ylt9
 
 #ffmpeg -i input.mp4 -i watermark.png -filter_complex "overlay" output.mp4
-ffmpeg -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-ffmpeg -i input.mp4 -b:v 64k output.mp4
-ffmpeg -i input.mp4 -filter:v "rotate=90" output.mp4
+ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -i input.mp4 -b:v 64k output.mp4
+ffmpeg -y -i input.mp4 -filter:v "rotate=90" output.mp4
+rm input.mp4
 
-youtube-dl -o input.mp4 https://www.dailymotion.com/video/x691qsf
+## ~35 mins, will take too long to process
+#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x691qsf
+#ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -i input.mp4 -r 30 output.mp4
+#ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+#rm input.mp4
 
-ffmpeg -i input.mp4 -vn -acodec copy output-audio.aac
-ffmpeg -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+youtube-dl -o input.mp4 https://www.dailymotion.com/video/x7zpeyp
+ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -i input.mp4 -r 30 output.mp4
+ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+rm input.mp4
 
-#ffmpeg -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+youtube-dl -o input.mp4 https://www.dailymotion.com/video/x4opm8u
+ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -i input.mp4 -r 30 output.mp4
+ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+rm input.mp4
 
-#ffmpeg -i input.mp4 -r 30 output.mp4
+youtube-dl -o input.mp4 https://www.dailymotion.com/video/x2xw86f
+ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -i input.mp4 -r 30 output.mp4
+ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+rm input.mp4
 
-#ffmpeg -i input.mp4 -aspect 1.7777 output.mp4
+#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x8i49ql
+#ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -i input.mp4 -r 30 output.mp4
+#ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+#rm input.mp4
 
+## ~20 mins, takes too long to process
+#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x5bavmu
+#ffmpeg -y -i input.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -i input.mp4 -r 30 output.mp4
+#ffmpeg -y -i input.mp4 -aspect 1.7777 output.mp4
+#rm input.mp4
 
 cd ..
 rm -rf ffmpeg
