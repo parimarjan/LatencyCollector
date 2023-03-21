a081fd84e75bf33b0f39199943f76db28afcedcc
parimarjan <parimarjannegi@gmail.com>
2023-03-21 03:25:15 -0400

    fix chess

--- scripts/linux_workloads/chess_trainingdata.sh
+++ scripts/linux_workloads/chess_trainingdata.sh
@@ -7,8 +7,9 @@ git submodule update --recursive --init
 cmake -H. -Bbuild
 cmake --build build
 
-#wget https://database.lichess.org/standard/lichess_db_standard_rated_2018-02.pgn.zst
-#unzstd lichess_db_standard_rated_2018-02.pgn.zst
+wget https://database.lichess.org/standard/lichess_db_standard_rated_2018-02.pgn.zst
+unzstd lichess_db_standard_rated_2018-02.pgn.zst
+rm lichess_db_standard_rated_2018-02.pgn.zst
 
 git clone https://github.com/MichaelB7/pgn-extract.git
 cd pgn-extract/src
@@ -18,5 +19,18 @@ cd ../..
 cat lichess_db_standard_rated_2018-02.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
 
 ./build/trainingdata-tool lichess_db_standard_rated_2018-02.pgn
+
+rm lichess_db_standard_rated_2018-02.pgn
+
+wget https://database.lichess.org/standard/lichess_db_standard_rated_2015-06.pgn.zst
+unzstd lichess_db_standard_rated_2015-06.pgn.zst
+rm lichess_db_standard_rated_2015-06.pgn.zst
+
+cat lichess_db_standard_rated_2015-06.pgn | ./pgn-extract/src/pgn-extract -7 -C -N --dropply 40 &> /dev/null
+
+./build/trainingdata-tool lichess_db_standard_rated_2015-06.pgn
+
+rm lichess_db_standard_rated_2015-06.pgn
+
 cd ..
 rm -rf trainingdata-tool
