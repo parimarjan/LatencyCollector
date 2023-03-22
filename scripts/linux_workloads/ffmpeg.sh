a4ce5f8dc3037fe8f2a7eed038663894acf73fc0
parimarjan <parimarjannegi@gmail.com>
2023-03-22 10:33:23 -0400

    updated things

--- scripts/linux_workloads/ffmpeg.sh
+++ scripts/linux_workloads/ffmpeg.sh
@@ -2,63 +2,63 @@
 mkdir -p ffmpeg
 cd ffmpeg
 
-youtube-dl -o input.mp4 https://www.dailymotion.com/video/x17ylt9
+#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x17ylt9
 
-#ffmpeg -i input.mp4 -i watermark.png -filter_complex "overlay" output.mp4
-ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-ffmpeg -y -nostdin -i input.mp4 -b:v 64k output.mp4
-ffmpeg -y -nostdin -i input.mp4 -filter:v "rotate=90" output.mp4
-rm input.mp4
+##ffmpeg -i input.mp4 -i watermark.png -filter_complex "overlay" output.mp4
+#ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -nostdin -i input.mp4 -b:v 64k output.mp4
+#ffmpeg -y -nostdin -i input.mp4 -filter:v "rotate=90" output.mp4
+#rm input.mp4
 
 ## ~35 mins, will take too long to process
-#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x691qsf
-#ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-#ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-#ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-#rm input.mp4
+#youtube-dl -o input2.mp4 https://www.dailymotion.com/video/x691qsf
+#ffmpeg -y -nostdin -i input2.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -nostdin -i input2.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -nostdin -i input2.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -nostdin -i input2.mp4 -r 30 output.mp4
+#ffmpeg -y -nostdin -i input2.mp4 -aspect 1.7777 output.mp4
+#rm input2.mp4
 
-youtube-dl -o input.mp4 https://www.dailymotion.com/video/x7zpeyp
-ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-rm input.mp4
+youtube-dl -o input3.mp4 https://www.dailymotion.com/video/x7zpeyp
+ffmpeg -y -nostdin -i input3.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -nostdin -i input3.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -nostdin -i input3.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -nostdin -i input3.mp4 -r 30 output.mp4
+ffmpeg -y -nostdin -i input3.mp4 -aspect 1.7777 output.mp4
+rm input3.mp4
 
-youtube-dl -o input.mp4 https://www.dailymotion.com/video/x4opm8u
-ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-rm input.mp4
+youtube-dl -o input4.mp4 https://www.dailymotion.com/video/x4opm8u
+ffmpeg -y -nostdin -i input4.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -nostdin -i input4.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -nostdin -i input4.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -nostdin -i input4.mp4 -r 30 output.mp4
+ffmpeg -y -nostdin -i input4.mp4 -aspect 1.7777 output.mp4
+rm input4.mp4
 
-youtube-dl -o input.mp4 https://www.dailymotion.com/video/x2xw86f
-ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-rm input.mp4
+youtube-dl -o input5.mp4 https://www.dailymotion.com/video/x2xw86f
+ffmpeg -y -nostdin -i input5.mp4 -vn -acodec copy output-audio.aac
+ffmpeg -y -nostdin -i input5.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+ffmpeg -y -nostdin -i input5.mp4 -vf scale=640:480,setsar=1 output.mp4
+ffmpeg -y -nostdin -i input5.mp4 -r 30 output.mp4
+ffmpeg -y -nostdin -i input5.mp4 -aspect 1.7777 output.mp4
+rm input5.mp4
 
-#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x8i49ql
-#ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-#ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-#ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-#rm input.mp4
+#youtube-dl -o input6.mp4 https://www.dailymotion.com/video/x8i49ql
+#ffmpeg -y -nostdin -i input6.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -nostdin -i input6.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -nostdin -i input6.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -nostdin -i input6.mp4 -r 30 output.mp4
+#ffmpeg -y -nostdin -i input6.mp4 -aspect 1.7777 output.mp4
+#rm input6.mp4
 
 ## ~20 mins, takes too long to process
-#youtube-dl -o input.mp4 https://www.dailymotion.com/video/x5bavmu
-#ffmpeg -y -nostdin -i input.mp4 -vn -acodec copy output-audio.aac
-#ffmpeg -y -nostdin -i input.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
-#ffmpeg -y -nostdin -i input.mp4 -vf scale=640:480,setsar=1 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -r 30 output.mp4
-#ffmpeg -y -nostdin -i input.mp4 -aspect 1.7777 output.mp4
-#rm input.mp4
+#youtube-dl -o input7.mp4 https://www.dailymotion.com/video/x5bavmu
+#ffmpeg -y -nostdin -i input7.mp4 -vn -acodec copy output-audio.aac
+#ffmpeg -y -nostdin -i input7.mp4 -codec:v libx264 -codec:a libmp3lame output.mkv
+#ffmpeg -y -nostdin -i input7.mp4 -vf scale=640:480,setsar=1 output.mp4
+#ffmpeg -y -nostdin -i input7.mp4 -r 30 output.mp4
+#ffmpeg -y -nostdin -i input7.mp4 -aspect 1.7777 output.mp4
+#rm input7.mp4
 
 cd ..
 rm -rf ffmpeg
--- scripts/linux_workloads/run_all.sh
+++ scripts/linux_workloads/run_all.sh
@@ -5,30 +5,30 @@ for ri in "${REPS[@]}"
   #bash run_single.sh fio.sh
   #bash run_single.sh iozone.sh
   #bash run_single.sh interbench.sh
-  bash run_single.sh cgrep.sh
+  bash run_single.sh pgrestores.sh
   bash run_single.sh ffmpeg.sh
   bash run_single.sh memtier.sh
-  bash run_single.sh tinymem.sh
+  #bash run_single.sh cgrep.sh
+  #bash run_single.sh tinymem.sh
   #bash run_single.sh chess_trainingdata.sh
-  bash run_single.sh pgrestores.sh
   #bash run_single.sh ripgrep.sh
   #bash run_single.sh scoutfish.sh
   #bash run_single.sh sysbench.sh
-  bash run_single.sh hayden.sh
-  bash run_single.sh lxml.sh
-  bash run_single.sh cpp_bench.sh
-  bash run_single.sh pbmc.sh
-  bash run_single.sh datasci_bench.sh
-  bash run_single.sh sentences.sh
-  bash run_single.sh matrix.sh
-  bash run_single.sh ansibench.sh
-  bash run_single.sh learned_sort.sh
-  bash run_single.sh percolation.sh
+  #bash run_single.sh hayden.sh
+  #bash run_single.sh lxml.sh
+  #bash run_single.sh cpp_bench.sh
+  #bash run_single.sh pbmc.sh
+  #bash run_single.sh datasci_bench.sh
+  #bash run_single.sh sentences.sh
+  #bash run_single.sh matrix.sh
+  #bash run_single.sh ansibench.sh
+  #bash run_single.sh learned_sort.sh
+  #bash run_single.sh percolation.sh
   #bash run_single.sh radix.sh
-  bash run_single.sh redis.sh
-  bash run_single.sh rambench.sh
-  bash run_single.sh pybench.sh
-  bash run_single.sh bonnie.sh
+  #bash run_single.sh redis.sh
+  #bash run_single.sh rambench.sh
+  #bash run_single.sh pybench.sh
+  #bash run_single.sh bonnie.sh
 done
 
 
