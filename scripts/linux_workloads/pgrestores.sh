49818d6156bf76db8a99779dbb36afcfd7d590a0
Ubuntu <ubuntu@ip-172-31-19-255.us-east-2.compute.internal>
2023-03-22 06:17:52 +0000

    pgrestore fix

--- scripts/linux_workloads/pgrestores.sh
+++ scripts/linux_workloads/pgrestores.sh
@@ -1,14 +1,7 @@
-./pg_setup.sh
-echo "postgres setup done"
-sleep 1
-source ~/.bashrc_exports
-
-createdb test
-sleep 3
-psql -d test < gen1.sql
-sleep 3
-
-#./pg_hint_setup.sh
+#./pg_setup.sh
+#echo "postgres setup done"
+#sleep 1
+#source ~/.bashrc_exports
 
 sleep 3
 ./imdb_setup.sh
